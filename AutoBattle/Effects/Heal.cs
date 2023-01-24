32ad34b3e7f899685a9e8ab8296f23b22eb9d23b
lucas-khaled <khaledbrugger3@gmail.com>
2023-01-23 23:02:12 -0300

    Create heal effect

---
+++ AutoBattle/Effects/Heal.cs
@@ -0,0 +1,27 @@
+﻿using AutoBattle.Characters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoBattle.Effects
+{
+    public class Heal : IEffect
+    {
+        private bool applied = false;;
+        private int healAmount;
+        public Heal(int healAmount)
+        {
+            this.healAmount = healAmount;
+        }
+
+        public bool ApplyEffect(Character character)
+        {
+            if (applied)
+                return false;
+
+            character.TakeDamage(-this.healAmount);
+            applied = true;
+            return true;
+        }
+    }
+}
