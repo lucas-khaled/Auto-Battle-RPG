9ed9c0da6cb42f43455512ee851d0dd0b03c954b
lucas-khaled <khaledbrugger3@gmail.com>
2023-01-23 21:35:40 -0300

    Adds Effect interface
    same principle that I used for Abilities

---
+++ AutoBattle/Effects/IEffect.cs
@@ -0,0 +1,12 @@
+﻿using AutoBattle.Characters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoBattle.Effects
+{
+    public interface IEffect
+    {
+        IEffect ApplyEffect(Character character);
+    }
+}
