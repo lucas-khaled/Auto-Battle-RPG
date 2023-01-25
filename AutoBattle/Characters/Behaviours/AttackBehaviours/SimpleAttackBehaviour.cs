d468d20bad851b5dc04b841de0133ad71460c5bc
lucas-khaled <khaledbrugger3@gmail.com>
2023-01-25 13:03:18 -0300

    Create SimpleAttackBehaviour
    Also adds range variable to attack behaviours

---
+++ AutoBattle/Characters/Behaviours/AttackBehaviours/SimpleAttackBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoBattle.Characters.Behaviours.AttackBehaviours
+{
+    public class SimpleAttackBehaviour : IAttackBehaviour
+    {
+        public int Range { get; }
+
+        private int variation;
+        private Random random = new Random();
+
+        public SimpleAttackBehaviour(int variation, int range) 
+        {
+            this.variation = variation;
+            this.Range = range;
+        }
+
+        public void Attack(Character character)
+        {
+            var baseDamage = character.BaseDamage;
+            var damage = random.Next(baseDamage - variation, baseDamage + variation + 1);
+            var target = character.Target;
+
+            Console.WriteLine($"{character.Name} attacked {target.Name} with {damage} of damage!");
+            target.TakeDamage(damage);
+        }
+    }
+}
