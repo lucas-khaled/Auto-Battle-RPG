c42e40b94faa30a2b2c56d2521d139e773ee1b00
lucas-khaled <khaledbrugger3@gmail.com>
2023-01-25 12:26:44 -0300

    Make gridObject starts in a not existing position
    Made that for the currentBox be ignored in first interaction

--- AutoBattle/GridObject.cs
+++ AutoBattle/GridObject.cs
@@ -9,7 +9,7 @@ namespace AutoBattle
     public abstract class GridObject
     {
         public string Name { get; protected set; }
-        public GridBox currentBox;
+        public GridBox currentBox = new GridBox(-1,-1, null, -1);
 
         public GridObject(string name) 
         {
