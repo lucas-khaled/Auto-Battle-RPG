5d08108048b176eddf33587749fdbc92b5757a7b
lucas-khaled <khaledbrugger3@gmail.com>
2023-01-25 21:29:49 -0300

    Create Frighten Ability and assign to Cleric class

---
+++ AutoBattle/Abilities/FrightenAbility.cs
@@ -0,0 +1,30 @@
+﻿using AutoBattle.Characters;
+using AutoBattle.Effects;
+using AutoBattle.GameManagement;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoBattle.Abilities
+{
+    public class FrightenAbility : ISpecialAbility
+    {
+        public string Name => "Fear Shout";
+
+        private int turnsOfWaiting = 5;
+        private int lastActivationTurn = 1;
+
+        public bool CanDoSpecial()
+        {
+            return GameManager.actualGame.Turn - lastActivationTurn > turnsOfWaiting;
+        }
+
+        public void DoSpecial(Character character)
+        {
+            lastActivationTurn = GameManager.actualGame.Turn;
+
+            Console.WriteLine($" - {character.Name} used {Name} on {character.Target.Name}".ToUpper());
+            character.Target.AddEffect(new Fear());
+        }
+    }
+}
