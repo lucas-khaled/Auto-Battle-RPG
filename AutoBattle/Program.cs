ab3e952873995aff7a4e3760da934c0b21c2ab6b
lucas-khaled <khaledbrugger3@gmail.com>
2023-01-26 21:23:49 -0300

    Update Program.cs

--- AutoBattle/Program.cs
+++ AutoBattle/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using static AutoBattle.Types;
 using AutoBattle.Characters;
 using AutoBattle.GameManagement;
+using System.Threading;
 
 namespace AutoBattle
 {
@@ -12,33 +13,7 @@ namespace AutoBattle
     {
         static void Main(string[] args)
         {
-            Grid grid = new Grid(5, 5);
-
-            Character player =  GetPlayerChoice();
-            Character enemy = CreateEnemyCharacter();
-            GameManager.StartNewGame(grid, player, enemy);
-
-            Character GetPlayerChoice()
-            {
-                Console.WriteLine("Choose Between One of this Classes:\n");
-                Console.WriteLine("[1] Paladin, [2] Warrior, [3] Cleric, [4] Archer");
-                
-                string choice = Console.ReadLine();
-                CharacterClass characterClass = (CharacterClass)int.Parse(choice);
-
-                return CharacterFactory.GenerateCharacter(characterClass, "Player");
-            }
-
-            Character CreateEnemyCharacter()
-            {
-                //randomly choose the enemy class and set up vital variables
-                var rand = new Random();
-                int randomInteger = rand.Next(1, 4);
-                CharacterClass enemyClass = (CharacterClass)randomInteger;
-                Console.WriteLine($"Enemy Class Choice: {enemyClass}");
-
-                return CharacterFactory.GenerateCharacter(enemyClass, "Enemy");
-            }
+            GameManager.StartNewGame();
         }
     }
 }
